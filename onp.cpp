5c2fe43cf5fd44c099959bdcda7eb3d2cd901ad2
Hamilkar247 <adonnibal96@gmail.com>
2021-06-04 23:32:48 +0200

    [WO-3] Dopisanie klasy stapel-a/stosu/stack-u
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- onp.cpp
+++ onp.cpp
@@ -7,8 +7,69 @@
 // i wyświetla wynik.
 
 #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAXIMUM 1000
+
+class Stapel {    //stos
+
+    int scheitel; //wierzchołek/top
+
+public:
+    int a[MAXIMUM];
+
+    Stapel() { scheitel = -1; }
+    bool druck(int x); //push/pchać
+    int aufziehen(); //pop/ciągnąć      
+    int aussehen();  //spojrzeć/look
+    bool istLeer();
+};
+
+bool Stapel::druck(int x) //pchać push
+{
+    if (scheitel >= (MAXIMUM - 1)) {
+        cout << "Stapel Uberlauf!"; //stack overflow / stos przepełniony
+        return false;
+    }
+    else {
+        a[++scheitel] = x;
+        cout << x << " hat zu Stapel gedrucken"; //pushed into stack // pchniete na stos
+        return true;
+    }
+}
+
+int Stapel::aufziehen() //ciąnąć pop
+{
+    if (scheitel < 0) {
+        cout << "Stapel Unterlauf"; //
+        return 0;
+    }
+    else {
+       int x = a[scheitel--];
+       return x;
+    }
+}
+
+int Stapel::aussehen()  //spojrzec /peek/look
+{
+    if(scheitel < 0) {
+        cout << "Stapel ist Leer";
+        return 0;
+    }
+    else {
+        int x = a[scheitel];
+        return x;
+    }
+}
+
+bool Stapel::istLeer()    //jest pusty/is Empty
+{
+    return (scheitel < 0); //wierzcholek
+}
 
 int main() {
-    std::cout << "Hallo, Welt!";
+    std::cout << "Hallo, Welt!\n";
     return 0;
 }
